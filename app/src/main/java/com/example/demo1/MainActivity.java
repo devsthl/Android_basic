b3fae4f0ad0785244941ae508ea81c253a3acd45
devsthl <ntuananhk54@gmail.com>
2021-10-08 16:59:07 +0700

    ex1

--- app/src/main/java/com/example/demo1/MainActivity.java
+++ app/src/main/java/com/example/demo1/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.demo1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnLogin, btnSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = (Button) findViewById(R.id.btnlogin);
+        btnSignup = (Button) findViewById(R.id.btnsingup);
+
+        btnLogin.setOnClickListener(this);
+        btnSignup.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.btnsingup) {
+            Intent intent = new Intent(MainActivity.this, SignupLazadaActivity.class);
+            startActivity(intent);
+        } else if (view.getId() == R.id.btnlogin) {
+
+        }
     }
 }
